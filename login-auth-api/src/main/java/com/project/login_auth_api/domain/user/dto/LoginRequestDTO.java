6419550399bfa074255265d0253bcf0204c12184
Gustavo Carvalho <gustavo.carvalho@urbana-pe.com.br>
2024-07-12 08:49:54 -0300

    Finally authentication with Spring Security

---
+++ login-auth-api/src/main/java/com/project/login_auth_api/domain/user/dto/LoginRequestDTO.java
@@ -0,0 +1,5 @@
+package com.project.login_auth_api.domain.user.dto;
+
+public record LoginRequestDTO(String email, String password) {
+
+}
---
+++ login-auth-api/src/main/java/com/project/login_auth_api/domain/user/dto/ResponseDTO.java
@@ -0,0 +1,5 @@
+package com.project.login_auth_api.domain.user.dto;
+
+public record ResponseDTO(String name, String token) {
+
+}
